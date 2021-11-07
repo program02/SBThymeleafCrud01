44a92664e851c93c6e893f78b81c568a9eb9184d
shahidur24 <shahidur2424@gmail.com>
2021-11-07 10:01:22 +0600

    SBThymeleafCrud using CrudRepository JPA SimpleExample

---
+++ src/main/java/com/exam/crud/SBrud1Application.java
@@ -0,0 +1,13 @@
+package com.exam.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SBrud1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SBrud1Application.class, args);
+	}
+
+}
